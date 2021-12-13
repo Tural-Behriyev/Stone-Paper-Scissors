60df6a179e5e01393675678cc53e3525d3d9175a
Behriyev <turalbaxriyev@gmail.com>
2021-12-13 13:51:14 +0400

    Scissors-Paper

---
+++ index.js
@@ -0,0 +1,25 @@
+//massiv anlayisi
+const siyahi=["kagiz.png","das.png","scissor.png"]
+
+//Dom doucumet object
+
+const kagiz=document.getElementById('kagiz')
+const das=document.getElementById('das')
+const qayci=document.getElementById('qayci')
+
+const komputerinsecimi=document.getElementById('komputer-secim')
+const neticeniyaz=document.getElementById('netice')
+
+//klikleme hadisesxi burada qeyde alinir
+kagiz.onclick=secimiele
+das.onclick=secimiele
+qayci.onclick=secimiele
+
+
+function secimiele() {
+
+}
+
+function randomsecim() {
+    
+}
